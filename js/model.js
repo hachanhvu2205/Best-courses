7848cd9f94a7350683ba45f233e821e91e451729
Anh Vu Hach <hachanhvu2205@gmail.com>
2020-06-28 11:49:12 +0700

    ok

--- js/model.js
+++ js/model.js
@@ -4,17 +4,17 @@ const currentUser = undefined
 const listCourses = undefined
 const currentCourse = undefined
 const currentVideo = undefined
-model.register = ({name, email, password }) => {
+model.register = ({ name, email, password }) => {
 
     //console.log(firstName)
     firebase.auth().createUserWithEmailAndPassword(email, password).then((res) => {
         console.log(res)
         firebase.auth().currentUser.sendEmailVerification()
         firebase.auth().currentUser.updateProfile({
-            displayName: name
-        })
-        //console.log(firebase.auth().currentUser)
-        view.setActiveScreen("loginScreen")
+                displayName: name
+            })
+            //console.log(firebase.auth().currentUser)
+            // view.setActiveScreen("loginScreen")
     }).catch((err) => {
         //console.log(err)
         alert(err.message)
@@ -22,7 +22,7 @@ model.register = ({name, email, password }) => {
 }
 
 model.login = (email, password) => {
-    firebase.auth().sigsignInWithEmailAndPassword(email, password).then((res) => {
+    firebase.auth().signInWithEmailAndPassword(email, password).then((res) => {
         if (!firebase.currentUser.emailVerified) {
             alert("Please verify email")
         } else {
@@ -32,20 +32,21 @@ model.login = (email, password) => {
 }
 
 model.search = (searchKey) => {
-    const res = firebase.firestore().collection('courses').where('title','==',searchKey).get()
+    model.loadCourses()
+    const res = firebase.firestore().collection('courses').where('title', '==', searchKey).get()
     model.currentCourse = getDataFromDocs(res.docs)[0]
 }
-model.loadCourses = async () => {
+model.loadCourses = async() => {
     const res = await firebase.firestore().collection('courses').get()
     model.listCourses = getDataFromDocs(res.docs)
-    model.currentCourse = listCourses[0]
+    model.currentCourse = model.listCourses[0]
 }
 
 
 getDataFromDoc = (doc) => {
-    let user = doc.data()
-    user.id = doc.id
-    return user
+    let course = doc.data()
+    course.id = doc.id
+    return course
 }
 
 getDataFromDocs = (docs) => {
