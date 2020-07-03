553626e1cd85cc1d4d2900e86c0889a0ab10e552
Anh Vu Hach <hachanhvu2205@gmail.com>
2020-07-03 18:56:00 +0700

    ok

--- js/component.js
+++ js/component.js
@@ -363,22 +363,7 @@ components.indexScreen = `
 <div class="add-products">
 <div class="container">
     <div class="add-products-row">
-        <div class="w3ls-add-grids">
-            <a href="products1.html">
-                <h4></h4>
-            </a>
-        </div>
-        <div class="w3ls-add-grids w3ls-add-grids-mdl">
-            <a href="products1.html">
-                <h4></h4>
 
-            </a>
-        </div>
-        <div class="w3ls-add-grids w3ls-add-grids-mdl1">
-            <a href="products.html">
-                <h4></h4>
-            </a>
-        </div>
         <div class="clerfix"> </div>
     </div>
 </div>
